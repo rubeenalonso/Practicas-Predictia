4859d964caef689fe2a40db6b812309451eaa7a4
rubeenalonso <ras639@alumnos.unican.es>
2024-02-02 11:01:55 +0100

    Nuevas ciudades

--- Tarea.py
+++ Tarea.py
@@ -18,17 +18,23 @@ subset_A = ds['pr'].sel(lat=38, lon=-0.5, method="nearest")
 
 subset_M= ds['pr'].sel(lat=40.5, lon=-3.5, method="nearest")
 
+# Selecciona los datos para Vigo
+
+subset_V = ds['pr'].sel(lat=42.5, lon=-8.5, method="nearest")
+
 # El dato de las 23.00 a 23.59 del día A se guarda como el primer dato del día B. Esto hay que modificarlo 3 ciudades
 
 subset_S["time"] = pd.to_datetime(subset_S["time"].values) - pd.Timedelta(hours=1)
 subset_A["time"] = pd.to_datetime(subset_A["time"].values) - pd.Timedelta(hours=1)
 subset_M["time"] = pd.to_datetime(subset_M["time"].values) - pd.Timedelta(hours=1)
+subset_V["time"] = pd.to_datetime(subset_V["time"].values) - pd.Timedelta(hours=1)
 
 # Pasar los datos de horario a diario 3 ciudades
 
 subset_S_diario = subset_S.resample(time="1D").sum()
 subset_A_diario = subset_A.resample(time="1D").sum()
 subset_M_diario = subset_M.resample(time="1D").sum()
+subset_V_diario = subset_V.resample(time="1D").sum()
 
 # Plot de los resultados 3 ciudades
 
@@ -47,11 +53,17 @@ subset_M_diario.plot()
 plt.title("Precipitación Madrid Enero 2020")
 plt.show()
 
+plt.figure()
+subset_V_diario.plot()
+plt.title("Precipitación Vigo Enero 2020")
+plt.show()
+
 # Precipitación acumulada 3 ciudades
 
 cumulative_subset_S = subset_S_diario.cumsum()
 cumulative_subset_A = subset_A_diario.cumsum()
 cumulative_subset_M = subset_M_diario.cumsum()
+cumulative_subset_V = subset_V_diario.cumsum()
 
 # Graficar las series acumulativas 3 ciudades
 
@@ -65,6 +77,10 @@ plt.show()
 
 cumulative_subset_M.plot(color='red')
 plt.title("Precipitación acumulada Madrid Enero 2020")
+plt.show()
+
+cumulative_subset_V.plot(color='red')
+plt.title("Precipitación acumulada Vigo Enero 2020")
 plt.show()
 
 
